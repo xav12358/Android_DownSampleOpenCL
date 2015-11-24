1175665cdb2c05622bd7b531a70feca061916726
Xavier <xavier.buhot@live.fr>
2015-11-24 22:03:22 +0100

    [add files]

---
+++ obj/local/armeabi/objs/JNIProcessor/processor.o.d
@@ -0,0 +1,72 @@
+obj/local/armeabi/objs/JNIProcessor/processor.o: jni/processor.cpp \
+ jni/processor.h \
+ /home/xavier/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/android/bitmap.h \
+ /home/xavier/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ /home/xavier/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/xavier/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/android/api-level.h \
+ /home/xavier/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/stdint.h \
+ /home/xavier/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/sys/_types.h \
+ /home/xavier/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/machine/_types.h \
+ /home/xavier/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/jni.h \
+ /home/xavier/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/android/log.h \
+ /home/xavier/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/string \
+ /home/xavier/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath \
+ /home/xavier/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/fstream \
+ /home/xavier/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/sstream \
+ /home/xavier/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstdlib \
+ jni/include/CL/cl.hpp jni/include/CL/opencl.h jni/include/CL/cl.h \
+ jni/include/CL/cl_platform.h jni/include/CL/cl_gl.h \
+ jni/include/CL/cl_gl_ext.h jni/include/CL/cl_ext.h \
+ /home/xavier/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/utility \
+ /home/xavier/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/vector \
+ /home/xavier/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstring
+
+jni/processor.h:
+
+/home/xavier/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/android/bitmap.h:
+
+/home/xavier/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+/home/xavier/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/xavier/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/android/api-level.h:
+
+/home/xavier/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/stdint.h:
+
+/home/xavier/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/sys/_types.h:
+
+/home/xavier/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/machine/_types.h:
+
+/home/xavier/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/jni.h:
+
+/home/xavier/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/android/log.h:
+
+/home/xavier/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/string:
+
+/home/xavier/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath:
+
+/home/xavier/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/fstream:
+
+/home/xavier/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/sstream:
+
+/home/xavier/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstdlib:
+
+jni/include/CL/cl.hpp:
+
+jni/include/CL/opencl.h:
+
+jni/include/CL/cl.h:
+
+jni/include/CL/cl_platform.h:
+
+jni/include/CL/cl_gl.h:
+
+jni/include/CL/cl_gl_ext.h:
+
+jni/include/CL/cl_ext.h:
+
+/home/xavier/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/utility:
+
+/home/xavier/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/vector:
+
+/home/xavier/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstring:
